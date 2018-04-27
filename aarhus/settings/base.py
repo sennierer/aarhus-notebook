8324aa71f06c696b58e575ab7ef357b33aaa3922
Unknown <m.schloegl@gmail.com>
2018-04-27 10:37:54 +0200

    moved the templates dir

--- aarhus/settings/base.py
+++ aarhus/settings/base.py
@@ -81,10 +81,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates/')
-#	'/Users/senmacbook/CloudStation/Aarhus/Code/aarhus/templates'
-    )
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static_dir/'),
